5acb305c4fd255f7cfa4dba3a75af74fd396b8dd
mylhh <928436972@qq.com>
2019-01-17 17:47:12 +0800

    主题选择初版（V1.0）
    滑动放大

--- pages/production/theme/theme.js
+++ pages/production/theme/theme.js
@@ -1,112 +1,43 @@
 
 // pages/production/result.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  onShow: function () {
-    this.getwidth();
-  },
-  //滑
-  get_index: function (e) {
-    var crash_current = e.detail.current;
-    var s = 0;
-    if (crash_current != 0 && crash_current != 1) {
-      s = parseInt(crash_current - 1) * this.data.s3_width;
-    }
-    this.setData({
-      c_index: e.detail.current,
-      scroll_left: s
-    });
-  },
-  //点
-  changeview: function (e) {
-    var crash_current = e.currentTarget.dataset.current;
-    var s = 0;
-    if (crash_current != 0 && crash_current != 1) {
-      s = parseInt(crash_current - 1) * this.data.s3_width;
-    }
-    this.setData({
-      c_index: e.currentTarget.dataset.current,
-      scroll_left: s
-    });
-  },
-  getwidth: function () {
-    var that = this;
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData(that.data.s3_width = res.windowWidth / 3);
-      },
-    })
-  },
-
-
-  //事件处理函数
-  ElementTap: function () {
-    wx.navigateTo({
-      url: '../element/element'
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+/*保证是中间图案的放大*/
+function chageIndex(index) {
+  if (index == 4) {
+    return 0;
+  } else {
+    return ++index;
   }
+}
+Page({
+  data: {
+    themeName:[
+      '繁花似锦',
+      '福寿天成',
+      '龙凤呈祥',
+      '鸟语花香',
+      '鸢飞鱼跃',
+    ],
+    imgUrls: [
+      '/images/production/theme/fhsj.jpg',
+      '/images/production/theme/lfcx1.jpg',
+      '/images/production/theme/yfyy.jpg',
+      '/images/production/theme/lfcx2.jpg',
+      '/images/production/theme/lfcx3.jpg',
+    ],
+    // swiper组件设置
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 2000,
+    vertical:true,
+    //初始状态就让中间的图案放大
+    swiperIndex:1
+  },
+  swiperChange(e) {
+    const that = this;
+    that.setData({
+      swiperIndex:chageIndex(e.detail.current),
+    })
+  },
 
 })
