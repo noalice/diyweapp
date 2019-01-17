c95fd351e5c9307bd367a9413768607e4494a248
stone <stonesry.888@qq.com>
2019-01-17 11:36:23 +0800

    初稿（element.wxml）

--- pages/production/element/element.js
+++ pages/production/element/element.js
@@ -6,6 +6,52 @@ Page({
    */
   data: {
 
+    c_index: 0,//当前
+    s3_width: 0,
+    t_width: 250,//选择按钮tab的宽度
+    scroll_left: 0,//上方滚动的位置
+    tab_tite_data: [
+      { "name": "1", "color": "orange", } ,
+      { "name": "2", "color": "blue", }, 
+      { "name": "3", "color": "green", }
+    ],
+
+  },
+
+  onShow: function () {
+    this.getwidth();
+  },
+  //滑
+  get_index: function (e) {
+    var crash_current = e.detail.current;
+    var s = 0;
+    if (crash_current != 0 && crash_current != 1) {
+      s = parseInt(crash_current - 1) * this.data.s3_width;
+    }
+    this.setData({
+      c_index: e.detail.current,
+      scroll_left: s
+    });
+  },
+  //点
+  changeview: function (e) {
+    var crash_current = e.currentTarget.dataset.current;
+    var s = 0;
+    if (crash_current != 0 && crash_current != 1) {
+      s = parseInt(crash_current - 1) * this.data.s3_width;
+    }
+    this.setData({
+      c_index: e.currentTarget.dataset.current,
+      scroll_left: s
+    });
+  },
+  getwidth: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData(that.data.s3_width = res.windowWidth / 3);
+      },
+    })
   },
 
   //事件处理函数
--- pages/production/theme/theme.js
+++ pages/production/theme/theme.js
@@ -8,6 +8,43 @@ Page({
 
   },
 
+  onShow: function () {
+    this.getwidth();
+  },
+  //滑
+  get_index: function (e) {
+    var crash_current = e.detail.current;
+    var s = 0;
+    if (crash_current != 0 && crash_current != 1) {
+      s = parseInt(crash_current - 1) * this.data.s3_width;
+    }
+    this.setData({
+      c_index: e.detail.current,
+      scroll_left: s
+    });
+  },
+  //点
+  changeview: function (e) {
+    var crash_current = e.currentTarget.dataset.current;
+    var s = 0;
+    if (crash_current != 0 && crash_current != 1) {
+      s = parseInt(crash_current - 1) * this.data.s3_width;
+    }
+    this.setData({
+      c_index: e.currentTarget.dataset.current,
+      scroll_left: s
+    });
+  },
+  getwidth: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData(that.data.s3_width = res.windowWidth / 3);
+      },
+    })
+  },
+
+
   //事件处理函数
   ElementTap: function () {
     wx.navigateTo({
